a47dc6c18359b5f43236ba207b13041744c302b2
Nashit Ansari <alogoforge100@gmail.com>
2026-09-15 11:32:28 +0530

    fix: change Dominica mobility title to Passport Dashboard with font-normal styling matching sharifgroup.ae

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -155,7 +155,7 @@
         }
     }
 
-    var I18N_VERSION = '20260915_v19';
+    var I18N_VERSION = '20260915_v20';
 
     function loadTranslation(lang, callback) {
         var pathname = (window.location && window.location.pathname) ? window.location.pathname.toLowerCase() : '';
