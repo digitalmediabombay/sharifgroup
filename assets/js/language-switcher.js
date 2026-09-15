870affbcce2b1c49282836f4e114b6986923b08c
Nashit Ansari <alogoforge100@gmail.com>
2026-09-15 11:56:14 +0530

    fix: change St. Kitts mobility title to Passport Dashboard matching live site

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -155,7 +155,7 @@
         }
     }
 
-    var I18N_VERSION = '20260915_v21';
+    var I18N_VERSION = '20260915_v22';
 
     function loadTranslation(lang, callback) {
         var pathname = (window.location && window.location.pathname) ? window.location.pathname.toLowerCase() : '';
