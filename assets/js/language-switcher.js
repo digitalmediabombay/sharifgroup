9a618a456df6473bd4815e8592528d6f35118698
Nashit Ansari <alogoforge100@gmail.com>
2026-09-20 00:49:05 +0530

    Fix blog consultation form: restore world map background, luxury italic heading, 2-column fields, and Register Inquiry button

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -203,7 +203,7 @@
         }
     }
 
-    var I18N_VERSION = '20260920_v11';
+    var I18N_VERSION = '20260920_v12';
 
     function loadTranslation(lang, callback) {
         var pathname = (window.location && window.location.pathname) ? window.location.pathname.toLowerCase() : '';
